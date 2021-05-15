f77c120b42d01aeb978be77e1271f48d595e91aa
Igr Alexánder Fernández Saúco <alexander.fernandez.sauco@gmail.com>
2021-05-15 19:32:19 -0400

    Update some results

--- src/StoneAssemblies.MassAuth.Benchmarks/MassAuthValidRulesScaleBenchmark.cs
+++ src/StoneAssemblies.MassAuth.Benchmarks/MassAuthValidRulesScaleBenchmark.cs
@@ -8,23 +8,24 @@ namespace StoneAssemblies.MassAuth.Benchmarks
 
     public class MassAuthValidRulesScaleBenchmark
     {
-        [Params(100)]
-        public int RulesCount { get; set; }
+        private readonly HttpClient httpClient = new HttpClient();
 
         [Params(100, 200)]
         public int ParallelRequests { get; set; }
 
+        [Params(100)]
+        public int RulesCount { get; set; }
+
         [Benchmark]
         public async Task TextMessageRequestAsync()
         {
-            using HttpClient httpClient = new HttpClient();
             var tasks = new List<Task>();
             var parallelRequests = this.ParallelRequests;
             for (var i = 0; i < parallelRequests; i++)
             {
                 var requestUri =
                     $"http://localhost:6004/api/Authorize/ValidTextMessage{this.RulesCount.ToString().PadLeft(3, '0')}?Text=12345";
-                var item = httpClient.GetAsync(requestUri);
+                var item = this.httpClient.GetAsync(requestUri);
                 tasks.Add(item);
             }
 
